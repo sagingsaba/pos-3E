51d083b59d22f4d91eeafe6fdbb6c8655720c42f
Neil Alfred Lapuz <154073022+sagingsaba@users.noreply.github.com>
2024-06-06 22:07:51 +0800

    Delete group3 directory

--- group3/js/nav.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.dropdown-btn').addEventListener('click', function (e) {
-    e.preventDefault();
-    var dropdown = this.parentNode;
-    dropdown.classList.toggle('active');
-});
