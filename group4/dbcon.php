d04488547ea52cdeb4557aa2394bcb5a9ffb53e1
Neil Alfred Lapuz <154073022+sagingsaba@users.noreply.github.com>
2024-06-06 22:13:06 +0800

    Delete group4 directory

--- group4/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try {
-        $pdoConnect = new PDO ("mysql:host=localhost:3306;dbname=$db","root","");
-        $pdoConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       
-    } catch (PDOException $exc) {
-        echo $exc->getMessage();
-    }
-?>
